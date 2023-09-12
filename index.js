76edbfc87cea1adde36489b93cf75904db909b90
“andre” <anressude2006@gmail.com>
2023-09-12 19:49:56 -0300

    remove /nomes

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const server = express();
-const nomes = require('./src/data/nomes.json');
 const pratos = require('./src/data/pratos.json');
 
 server.get('/', (req,res) => {
-    return res.json("André lindo")
+    return res.json("API do Personal Nutri")
 })
 
 server.get('/pratos', (req,res) => {
     return res.json(pratos);
-});
-server.get('/nomes', (req,res) => {
-    return res.json(nomes);
 });
 
 server.listen(3000, () =>{
