da031e65369f9a5b9882d88e8f0e8cad7ba0ba94
“andre” <anressude2006@gmail.com>
2024-08-24 16:45:15 -0300

    atualizando api

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const server = express();
 const pratos = require('./src/data/pratos.json');
 
+server.use(cors());
+
 server.get('/', (req,res) => {
-    return res.json("API do Personal Nutri")
+    return res.json("API Personal Nutri")
 })
 
 server.get('/pratos', (req,res) => {
@@ -11,5 +13,5 @@ server.get('/pratos', (req,res) => {
 });
 
 server.listen(3000, () =>{
-    console.log('Servidor está funcionando...');
+    console.log('Servidor em funcionamento...');
 });
